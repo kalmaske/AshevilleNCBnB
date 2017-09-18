f381707a42ba0f62d8026fd1fec8806327393f0a
kalmaske <kalmaske@uncc.edu>
2017-09-17 23:54:05 -0400

    Ghraham's part added

--- controllers/controller.js
+++ controllers/controller.js
@@ -53,4 +53,49 @@ router.post("/home", function (req, res) {
 });
 
 
+// ===================================
+// new search get
+router.put("/search", function (req, res) {
+  db.asheville_data.findAll({}).then(function (data) {
+    var hbsObject = {
+      asheville_data: data
+    };
+    console.log(hbsObject);
+    res.render("search", hbsObject);
+  });
+});
+
+router.get("/search", function (req, res) {
+  db.listings.findAll({}).then(function (data) {
+    var hbsObject = {
+      listings: data
+    };
+    console.log(hbsObject);
+    res.render("search", hbsObject);
+  });
+});
+
+router.post("/search", function (req, res) {
+  var listing = {    
+    host_name: req.body.host_name,
+    name: req.body.name,
+    neighbourhood: req.body.neighbourhood,
+    price: req.body.price,
+    room_type: req.body.room_type,
+    minimum_nights: req.body.minimum_nights,
+    availability_365: req.body.availability_365
+  };
+
+
+  db.asheville_data
+    .create(listing)
+    .then(function () {
+      res.redirect("/search");
+    })
+    .catch(function (err) {
+      console.error(err);
+    });
+});
+
+
 module.exports = router;
